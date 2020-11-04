47a8fbbfbbb9e21d8702108423fe64c6c76e7c1d
Vatsal2206 <71867953+Vatsal2206@users.noreply.github.com>
2020-11-04 13:20:53 +0530

    Add files via upload

---
+++ box1.js
@@ -0,0 +1,28 @@
+class Box1{
+    constructor(x,y){
+        var options = {
+            'isStatic' : false,
+            'density' : 1.2,
+            'friction' : 0.2,
+            'restitution' : 1
+        }
+        this.block = Bodies.rectangle(x,y,30,30,options)
+        this.width = 30;
+        this.height = 30;
+        World.add(world,this.block)
+    }
+    display(){
+        var angle = this.block.angle;
+        
+        push();
+        fill(232, 81, 67)
+        strokeWeight(1) 
+        stroke(0)
+        translate(this.block.position.x, this.block.position.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0, 0, this.width, this.height);
+        pop();
+        
+    }
+}
